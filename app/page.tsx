03604d7eb92f7b21ca533b0d829897e97be7523d
tylujianan <ljn199379@icloud.com>
2025-02-06 23:35:46 +0800

    clean unused props

--- app/page.tsx
+++ app/page.tsx
@@ -33,11 +33,11 @@ export default function Home() {
   return (
     <>
       <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
-      <Header isDarkMode={isDarkMode}/>
+      <Header/>
       <About isDarkMode={isDarkMode}/>
-      <Services isDarkMode={isDarkMode}/>
+      <Services/>
       <Work isDarkMode={isDarkMode}/>
-      <Contact isDarkMode={isDarkMode}/>
+      <Contact/>
       <Footer isDarkMode={isDarkMode}/>
     </>
   );
