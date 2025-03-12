f874f4fd7ccb8ef30ed5221277361febf5cb7a07
tylujianan <ljn199379@icloud.com>
2025-03-12 12:42:47 +0800

    fix in 4.0

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -31,7 +31,7 @@ const Services = () => {
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
                 transition={{ duration: 0.6, delay: 0.9 }}
-                className='grid grid-cols-auto gap-6 my-10'>
+                className='grid md:flex gap-6 my-10'>
                 {serviceData.map(({ icon, title, description, link }, index) => (
                     <motion.div
                         whileHover={{ scale: 1.05 }}
