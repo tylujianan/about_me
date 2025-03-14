98592cc07463611427cb715d3c539b621072f2a8
tylujianan <ljn199379@icloud.com>
2025-03-14 12:58:04 +0800

    book component

---
+++ app/components/Book/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
+import './book.css'
+
+// interface IBookData {
+//     src?: string,
+//     content?: string 
+// }
+
+interface IProps {
+    type?: 'Image' | 'Content',
+    component?: any,
+    dataList: Array<any>,
+    hoverMoveX?: string,
+}
+
+export default function Book(props: IProps) {
+    return (
+        <div>
+            {/* <ul className='flip_book list-none w-80 h-100 duration-1500 transform-3d perspective-[900px] hover:translate-x-[150px] hover:nth-[1]:-rotate-y-180 hover:nth-[1]:duration-1400 hover:nth-[2]:-rotate-y-180 hover:nth-[2]:duration-2000 hover:nth-[3]:-rotate-y-180 hover:nth-[3]:duration-2600 hover:nth-[4]:-rotate-y-180 hover:nth-[4]:duration-3200 hover:nth-[5]:-rotate-y-180 hover:nth-[5]:duration-3800 hover:nth-[6]:-rotate-y-180 hover:nth-[6]:duration-4400'> */}
+            {
+                props.type === 'Content' && props.component ? <ul>{props.component()}</ul> :
+                    <ul className={`flip_book list-none w-80 h-100 duration-1500 transform-3d perspective-[900px] hover:translate-x-[${props.hoverMoveX || 0}]`}>
+                        {
+                            props.dataList.map((item, index) => {
+
+                                return <li className={`absolute w-full h-full origin-left -rotate-y-${25 - index * 2} duration-${2000 - 300 * index}`} key={index}><Image className='w-full h-full shadow-black' alt={`第${index + 1}页`} src={item} /></li>
+
+                            })
+                        }
+                    </ul>
+            }
+
+        </div>
+    )
+}
--- app/diary/detail/[id]/page.tsx
+++ app/diary/detail/[id]/page.tsx
@@ -1,20 +1,26 @@
-import Image from 'next/image'
-import React from 'react'
+import Book from '@/app/components/Book'
 import { assets } from '@/assets/assets'
-import './book.css'
 
 export default function Page() {
+    const ImageList = [assets.book1, assets.book2, assets.book3, assets.book4, assets.book5, assets.book6, assets.book7];
+    const ContentList = ['aaaaaaa', 'bbbbbb', 'cccccccc', 'ddddddd', 'fffffff']
+    const ContentPage = () => (
+        <>
+            {ContentList.reverse().map((item, index) => (
+                <li key={index} className={`absolute w-full h-full bg-white border-[0.5px] border-grey-400`}>{item}</li>
+            ))}
+        </>
+    );
     return (
-        // <ul className='my-diary list-none w-80 h-100 duration-1500 transform-3d perspective-[900px] hover:translate-x-[150px] hover:nth-[1]:-rotate-y-180 hover:nth-[1]:duration-1400 hover:nth-[2]:-rotate-y-180 hover:nth-[2]:duration-2000 hover:nth-[3]:-rotate-y-180 hover:nth-[3]:duration-2600 hover:nth-[4]:-rotate-y-180 hover:nth-[4]:duration-3200 hover:nth-[5]:-rotate-y-180 hover:nth-[5]:duration-3800 hover:nth-[6]:-rotate-y-180 hover:nth-[6]:duration-4400'>
-        <ul className='my-diary list-none w-80 h-100 duration-1500 transform-3d perspective-[900px]'>
-            <li className='absolute origin-left -rotate-y-25 duration-2000' key='1'><Image className='w-80 h-100 shadow-black' alt={`第1页`} src={assets.book1} /></li>
-            <li className='absolute origin-left -rotate-y-23 duration-1700' key='2'><Image className='w-80 h-100 shadow-black' alt={`第2页`} src={assets.book2} /></li>
-            <li className='absolute origin-left -rotate-y-21 duration-1400' key='3'><Image className='w-80 h-100 shadow-black' alt={`第3页`} src={assets.book3} /></li>
-            <li className='absolute origin-left -rotate-y-19 duration-1100' key='4'><Image className='w-80 h-100 shadow-black' alt={`第4页`} src={assets.book4} /></li>
-            <li className='absolute origin-left -rotate-y-17 duration-800' key='5'><Image className='w-80 h-100 shadow-black' alt={`第5页`} src={assets.book5} /></li>
-            <li className='absolute origin-left -rotate-y-15 duration-500' key='6'><Image className='w-80 h-100 shadow-black' alt={`第6页`} src={assets.book6} /></li>
-            <li className='absolute origin-left -rotate-y-13' key='7'><Image className='w-80 h-100 shadow-black' alt={`第7页`} src={assets.book7} /></li>
-        </ul>
-
+        <Book {...{
+            dataList: ImageList,
+            hoverMoveX: '150px'
+        }}/>
+        // <Book {...{
+        //     type: 'Content',
+        //     dataList: ContentList,
+        //     hoverMoveX: '150px',
+        //     component: ContentPage
+        // }} />
     )
 }
