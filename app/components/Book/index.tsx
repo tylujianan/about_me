853e06fbbbeb36429905931198debffc0d9fa6ae
tylujianan <ljn199379@icloud.com>
2025-04-07 18:28:05 +0800

    use trae fix build bug

--- app/components/Book/index.tsx
+++ app/components/Book/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import { assets } from '@/assets/assets'
 import './book.css'
 
 // interface IBookData {
--- app/diary/detail/[id]/page.tsx
+++ app/diary/detail/[id]/page.tsx
@@ -3,14 +3,14 @@ import { assets } from '@/assets/assets'
 
 export default function Page() {
     const ImageList = [assets.book1, assets.book2, assets.book3, assets.book4, assets.book5, assets.book6, assets.book7];
-    const ContentList = ['aaaaaaa', 'bbbbbb', 'cccccccc', 'ddddddd', 'fffffff']
-    const ContentPage = () => (
-        <>
-            {ContentList.reverse().map((item, index) => (
-                <li key={index} className={`absolute w-full h-full bg-white border-[0.5px] border-grey-400`}>{item}</li>
-            ))}
-        </>
-    );
+    // const ContentList = ['aaaaaaa', 'bbbbbb', 'cccccccc', 'ddddddd', 'fffffff']
+    // const ContentPage = () => (
+    //     <>
+    //         {ContentList.reverse().map((item, index) => (
+    //             <li key={index} className={`absolute w-full h-full bg-white border-[0.5px] border-grey-400`}>{item}</li>
+    //         ))}
+    //     </>
+    // );
     return (
         <Book {...{
             dataList: ImageList,
